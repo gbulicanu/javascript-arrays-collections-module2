a75ccee69c9dfb64e0f798f35b84f0380f082fdf
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-05-23 10:39:21 +0300

    Other plays with array spread operator

--- js/page.js
+++ js/page.js
@@ -2,7 +2,7 @@ let ctx = document.getElementById("monthlySales").getContext("2d");
 let pieCtx = document.getElementById("deptSales").getContext("2d");
 let yearlyLabel = document.getElementById("yearlyTotal");
 
-let monthlySales = Array.of(12, 9, 3);
+let monthlySales = Array.of(1200, 9000, 3000);
 let monthlyLabels = Array.of("Oct", "Nov", "Dec");
 
 let deptSales = Array.of(12, 9, 3);
@@ -11,6 +11,13 @@ let deptLabels = Array.of("Hiking", "Running", "Hunting");
 function addYearlyTotal(a, b, c) {
   return a + b + c;
 }
+
+let octNumbers = Array.of(1200, 1000, 9000);
+let novNumbers = Array.of(1100, 2000, 9000);
+let decNumbers = Array.of(4000, 1000, 5000);
+
+let total = Array.of(...octNumbers, ...novNumbers, ...decNumbers);
+alert(total);
 
 let yearlyTotal = addYearlyTotal(...monthlySales);
 yearlyLabel.innerHTML = "$" + yearlyTotal;
